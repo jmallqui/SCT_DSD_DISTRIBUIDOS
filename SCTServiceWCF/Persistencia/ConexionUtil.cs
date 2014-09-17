e95b0952c468997ae6e15e6c9698c6facc5893b5
jmallqui <jmallqui@gmail.com>
2014-09-17 10:57:45 -0500

    version 1.0

---
+++ SCTServiceWCF/Persistencia/ConexionUtil.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AsesoresService.Persistencia
+{
+    public class ConexionUtil
+    {
+        public static string ObtenerCadena()
+        {
+            return "Data Source=(local);Initial Catalog=BD_Asesores;Integrated Security=SSPI;";
+        }
+    }
+}
---
+++ SCTServiceWCF/Servicios/Centros.svc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SCTServiceWCF.Servicios
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Centros" en el código, en svc y en el archivo de configuración a la vez.
+    public class Centros : ICentros
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
